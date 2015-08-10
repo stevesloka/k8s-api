bfae37504ab51aa69dbff2d955a063c52d18b4c1
Steve Sloka <steve@stevesloka.com>
2015-08-10 16:01:42 -0400

    added thread sleep

--- src/main/java/demo/DemoApplication.java
+++ src/main/java/demo/DemoApplication.java
@@ -9,9 +9,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication  {
 
-
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        Thread.sleep(5000);
         SpringApplication.run(DemoApplication.class, args);
     }
 
